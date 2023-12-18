a9f5b00c371801e95935b9f72ac27ae3b8f35465
VISH01AL <g.vish1al@gmail.com>
2023-12-17 23:08:47 -0500

    added 2.3

---
+++ 2.3-simulating mass/index.js
@@ -0,0 +1,9 @@
+const body=document.getElementsByTagName("body")[0]
+const canvas=document.createElement("canvas")
+body.appendChild(canvas)
+canvas.width=innerWidth
+canvas.height=innerHeight
+
+function animate(){
+    requestAnimationFrame(animate)
+}
