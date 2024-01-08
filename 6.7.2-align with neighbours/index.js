c0856c476d35a355a81a21080b9663f2e0f39fbb
VISH01AL <g.vish1al@gmail.com>
2024-01-08 18:45:52 -0500

    started 6.7.2

---
+++ 6.7.2-align with neighbours/index.js
@@ -0,0 +1,105 @@
+import {Pvector, getRandomColor} from './helper.js'
+const body=document.getElementsByTagName("body")[0]
+const canvas=document.createElement("canvas")
+const c=canvas.getContext("2d")
+body.appendChild(canvas)
+canvas.width=innerWidth
+canvas.height=innerHeight
+
+
+
+class Agent{
+constructor(x,y){
+    this.location=new Pvector(x,y)
+    this.velocity=new Pvector(2,2)
+    this.acceleration=new Pvector(0,0)
+    this.radius=15
+    this.velocitylimit=3
+    this.color=getRandomColor()
+    // this.forcelimit=0.1
+}
+applyForce(force){
+    // force.limit(this.forcelimit)
+    this.acceleration.add(force)
+}
+align(){
+
+}
+update(){
+    this.velocity.add(this.acceleration)
+    this.velocity.limit(this.velocitylimit)
+    this.location.add(this.velocity)
+    this.acceleration.setmag(0)
+    this.boundary()
+}
+boundary(){
+    if(this.location.x>=innerWidth){
+        this.location.x=1
+    }
+    if(this.location.x<=0){
+        this.location.x=innerWidth-1
+    }
+    if(this.location.y>=innerHeight){
+        this.location.y=1
+    }
+    if(this.location.y<=0){
+        this.location.y=innerHeight-1
+    }
+}
+draw(c){
+    c.beginPath()
+    c.arc(this.location.x,this.location.y,this.radius,0,Math.PI*2,false)
+    c.fillStyle=this.color
+    c.strokeStyle=this.color
+    c.fill()
+    c.stroke()
+}
+}
+
+
+
+
+let isDragging = false;
+let offsetX, offsetY;
+
+function handleMouseDown(e) {
+  isDragging = true;
+  offsetX = e.clientX - canvas.getBoundingClientRect().left;
+  offsetY = e.clientY - canvas.getBoundingClientRect().top;
+
+  document.addEventListener('mousemove', handleMouseMove);
+  document.addEventListener('mouseup', handleMouseUp);
+}
+
+function handleMouseMove(e) {
+  if (isDragging) {
+agarr.push(new Agent(e.clientX,e.clientY))
+  }
+}
+
+function handleMouseUp() {
+  isDragging = false;
+  document.removeEventListener('mousemove', handleMouseMove);
+  document.removeEventListener('mouseup', handleMouseUp);
+}
+
+canvas.addEventListener('mousedown', handleMouseDown);
+
+
+
+
+
+let agarr=[]
+agarr.push(new Agent(innerWidth/2,innerHeight/2))
+
+function animate(){
+    c.clearRect(0,0,innerWidth,innerHeight)
+    requestAnimationFrame(animate)
+    for(let i=0;i<agarr.length;i++){
+        agarr[i].update()
+        agarr[i].draw(c)
+    }
+    
+}   
+
+animate()
